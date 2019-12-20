3036f9f8f8821d2abef93ee51a45aa542699b752
Yasha Bubnov <ybubnov@gojuno.com>
2019-12-20 15:53:47 +0300

    Add basic server implementation.

--- server.go
+++ server.go
@@ -1,63 +1,159 @@
 package resly
 
-// Server is an HTTP handler used to serve GraphQL requests.
+import (
+	"net/http"
+
+	"github.com/graphql-go/handler"
+)
+
+// Server is a handler used to serve GraphQL requests.
 //
 // Example of basic handler with a complex type:
 //
+//	type PostInput struct {
+//		AuthorID string `json:"author_id"`
+//		Text     string `json:"text"`
+//	}
+//
+//	type Post struct {
+//		ID       string `json:"id"`
+//		AuthorID string `json:"author_id"`
+//		Text     string `json:"text"`
+//	}
+//
+//	type Author struct {
+//		ID   string `json:"id"`
+//		Name string `json:"name"`
+//	}
+//
 //	s := resly.Server{
 //		Type: []resly.TypeDef{
-//			resly.NewType(Post{}, resly.Methods{
+//			resly.NewType(Post{}, resly.Funcs{
 //				"author": func(ctx context.Context, p Post) (Author, error) {
 //					// Fetch author.
+//					return Author{}, nil
 //				},
 //			}),
 //		},
-//		Query: map[string]FuncDef{
+//		Queries: []resly.FuncDefs{
+//			resly.NewFunc("posts", func(ctx context.Context) ([]Post, error) {
+//				// Fetch all posts.
+//				return nil, nil
+//			}),
+//			resly.NewFunc("authors", func(ctx context.Context) ([]Author, error) {
+//				// Fetch all authors.
+//				return nil, nil
+//			}),
+//		},
+//		Mutations: []resly.FuncDefs{
+//			resly.NewFunc("createPost", func(ctx context.Context, p PostInput) (Post, error) {
+//				// Create a new post.
+//				return Post{}, nil
+//			},
 //		},
 //	}
 //
-//	http.Handle("/graphql", s.Handler())
+//	http.Handle("/graphql", s.MustCreateHandler())
+//	http.ListenAndServe(":8080", nil)
 //
 type Server struct {
-	Type     map[string]TypeDef
-	Query    map[string]FuncDef
-	Mutation map[string]FuncDef
+	Types     []TypeDef
+	Queries   []FuncDef
+	Mutations []FuncDef
 }
 
-// type Server struct {
-// 	graphql GraphQLCompiler
-// }
+// AddType adds given type definitions in the list of the types.
+func (s *Server) AddType(typedef TypeDef) {
+	s.Types = append(s.Types, typedef)
+}
+
+// AddQuery adds given function definition in the list of queries.
+func (s *Server) AddQuery(funcdef FuncDef) {
+	s.Queries = append(s.Queries, funcdef)
+}
+
+// AddMutation adds given function definition in the list of mutations.
 //
-// func (s *Server) ResolveType(typedef TypeDef) {
-// 	if err := s.graphql.AddType(typedef); err != nil {
-// 		panic(err)
-// 	}
-// }
+// Note, that GraphQL does not allow to use the same type as input and as
+// an output within a single mutation. Therefore a common practice is to
+// define _input_ types as mutation input.
 //
-// func (s *Server) ResolveQuery(funcdef FuncDef) {
-// 	if err := s.graphql.AddQuery(funcdef); err != nil {
-// 		panic(err)
-// 	}
-// }
+// Example of separation function input and output:
 //
-// func (s *Server) ResolveMutation(funcdef FuncDef) {
-// 	if err := s.graphql.AddMutation(funcdef); err != nil {
-// 		panic(err)
-// 	}
-// }
+//	// PostInput lists arguments required to create a post.
+//  //
+//  // Such separation is required since user cannot generate identifier
+//	// of the post, it's the responsibility of the server.
+//	type PostInput struct {
+//		AuthorID string `json:"author_id"`
+//		Text     string `json:"text"`
+//	}
 //
-// func (s *Server) ListenAndServe(addr string) {
-// 	schema, err := s.graphql.Compile()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+//  // Post is the model of the post.
+//	type Post struct {
+//		ID       string `json:"id"`
+//		AuthorID string `json:"author_id"`
+//		Text     string `json:"text"`
+//	}
 //
-// 	h := graphqlhandler.New(&graphqlhandler.Config{
-// 		Schema:   &schema,
-// 		Pretty:   true,
-// 		GraphiQL: true,
-// 	})
+//	mut := resly.NewFunc("createPost", func(ctx context.Context, p PostInput) (Post, error) {
+//		// Insert a post into the database.
+//		post := Post{
+//			ID:       uuid.New(),
+//			AuthorID: p.AuthorID,
+//			Text:     p.Text,
+//		}
+//		db.Insert(post)
+//		return post, nil
+//	})
+func (s *Server) AddMutation(funcdef FuncDef) {
+	s.Mutations = append(s.Mutations, funcdef)
+}
+
+// CreateHandler creates a new HTTP handler used to process GraphQL requests.
 //
-// 	http.Handle("/graphql", h)
-// 	http.ListenAndServe(addr, nil)
-// }
+// This function registers all type and function definitions in the GraphQL
+// schema. Produced schema will be used to resolve requests.
+//
+// On duplicate types, queries or mutations, function returns an error.
+func (s *Server) CreateHandler() (http.Handler, error) {
+	var graphql GraphQLCompiler
+
+	// Register all defined types and functions within a GraphQL compiler.
+	for _, typedef := range s.Types {
+		if err := graphql.AddType(typedef); err != nil {
+			return nil, err
+		}
+	}
+	for _, funcdef := range s.Queries {
+		if err := graphql.AddQuery(funcdef); err != nil {
+			return nil, err
+		}
+	}
+	for _, funcdef := range s.Mutations {
+		if err := graphql.AddMutation(funcdef); err != nil {
+			return nil, err
+		}
+	}
+
+	schema, err := graphql.Compile()
+	if err != nil {
+		return nil, err
+	}
+
+	return handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	}), nil
+}
+
+// MustCreateHandler executes CreateHandler, but panics, when the result
+// produces an error.
+func (s *Server) MustCreateHandler() http.Handler {
+	h, err := s.CreateHandler()
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
